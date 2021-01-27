ea36b72c9ede45eda82f99f1d4e2dc14594d4ac2
David J Sealey <david.sealey@soak.co.uk>
2021-01-27 11:23:11 +0000

    Feature: Tweaks to buttons

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -3,12 +3,22 @@ import Button from '../components/Button';
 export default {
   title: 'Components/Button',
   component: Button,
+  argTypes: {
+    styles: {
+      table: {
+        disable: true
+      }
+    }
+  }
 }
 
-const Template = (args) => <Button {...args} />;
+const Template = args => <Button {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = { href: "https://www.example.com", styles: 'bg-white text-blue-500', label: "Click Me", disabled: false};
+Primary.args = { href: "https://www.example.com", styles: 'bg-white border-blue-500 w-full text-blue-500', label: "Click Me", disabled: false};
 
 export const Active = Template.bind({});
-Active.args = { ...Primary.args, styles: "bg-blue-500 text-white" };
+Active.args = { ...Primary.args, styles: "bg-blue-500 border-blue-500 w-full text-white" };
+
+export const Action = Template.bind({});
+Action.args = { ...Primary.args, styles: "border-black bg-black text-white uppercase", label: "Complete" };
